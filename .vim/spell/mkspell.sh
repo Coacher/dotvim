29ff034f19603a9145097661379fcd2d5aa3bf83
Ilya Tumaykin <itumaykin@gmail.com>
2025-12-11 15:08:48 +0300

    mkspell: detect encoding instead of hardcoding

--- .vim/spell/mkspell.sh
+++ .vim/spell/mkspell.sh
@@ -4,14 +4,14 @@ shopt -s nullglob
 
 cp /usr/share/hunspell/en_{GB,US}.* .
 >en.words
-unmunch en_GB.dic en_GB.aff | iconv -f ISO-8859-1 -t UTF-8 >> en.words
-unmunch en_US.dic en_US.aff | iconv -f UTF-8 -t UTF-8 >> en.words
+unmunch en_GB.dic en_GB.aff | iconv -f $(uchardet en_GB.aff) -t UTF-8 >> en.words
+unmunch en_US.dic en_US.aff | iconv -f $(uchardet en_US.aff) -t UTF-8 >> en.words
 sort --stable --unique --output=en.words en.words
 rm *.dic *.aff
 
 cp /usr/share/hunspell/ru_RU.* .
 >ru.words
-unmunch ru_RU.dic ru_RU.aff | iconv -f KOI8-R -t UTF-8 >> ru.words
+unmunch ru_RU.dic ru_RU.aff | iconv -f $(uchardet ru_RU.aff) -t UTF-8 >> ru.words
 sort --stable --unique --output=ru.words ru.words
 rm *.dic *.aff
 
