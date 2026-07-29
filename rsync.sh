473327719f2855f2c70444e864fa8c675a5ea1de
Ilya Tumaykin <itumaykin@gmail.com>
2026-07-29 07:46:01 +0300

    rsync: Debian 13 compat
    Older rclone there.

--- rsync.sh
+++ rsync.sh
@@ -6,9 +6,10 @@ SRC="$(dirname "$(readlink -e "${0}")")/.vim/"
 TGT="${1}"
 
 rclone check "${SRC}" "${TGT}" \
-    --one-way --links --log-file /dev/null --combined - | \
-    rg -v '^=' | sort -k 2
+    --one-way --links \
+    --log-file /dev/null --combined - \
+    | rg -v '^=' --sort=path
 
 rclone copy "${SRC}" "${TGT}" \
-    --checksum --links --metadata --no-update-dir-modtime --no-update-modtime \
+    --checksum --links --metadata --no-update-modtime \
     --log-file /dev/null --interactive
